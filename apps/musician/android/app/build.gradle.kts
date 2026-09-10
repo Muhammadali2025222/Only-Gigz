71eead765d4483a0e56b82a6bd0a282445bf04d2
saeeedmuhammadali2003-tech <muhammadali2025222@gmail.com>
2026-09-11 00:06:14 +0500

    fix(musician): use dynamic versionName for APK rename in build.gradle.kts

--- apps/musician/android/app/build.gradle.kts
+++ apps/musician/android/app/build.gradle.kts
@@ -49,10 +49,10 @@ tasks.whenTaskAdded {
         doLast {
             val apkDir = file("${buildDir}/outputs/flutter-apk")
             val oldFile = file("${apkDir}/app-release.apk")
-            val newFile = file("${apkDir}/OnlyGigz-Musician-v1.0.0.apk")
+            val newFile = file("${apkDir}/OnlyGigz-Musician-v${android.defaultConfig.versionName}.apk")
             if (oldFile.exists()) {
                 oldFile.renameTo(newFile)
-                println("APK renamed to: OnlyGigz-Musician-v1.0.0.apk")
+                println("APK renamed to: ${newFile.name}")
             }
         }
     }
